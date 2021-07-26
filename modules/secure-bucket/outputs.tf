8a873a916516661405ee6854b9823b669a4b11c4
Kut Akdogan <kut.akdogan@gmail.com>
2021-07-26 14:31:04 -0400

    Fix more outputs

--- modules/secure-bucket/outputs.tf
+++ modules/secure-bucket/outputs.tf
@@ -3,7 +3,8 @@ output "this_bucket" {
   value       = var.enabled ? aws_s3_bucket.content[0] : null
 }
 
-output "log_bucket" {
-  description = "The S3 bucket used for storing access logs of this bucket."
-  value       = var.enabled ? aws_s3_bucket.access_log[0] : null
-}
+# TODO: turn back on
+//output "log_bucket" {
+//  description = "The S3 bucket used for storing access logs of this bucket."
+//  value       = var.enabled ? aws_s3_bucket.access_log[0] : null
+//}
