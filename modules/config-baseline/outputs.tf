9169c4259caaa2fd9b6cbe0f1ace05167e70af5b
Kut Akdogan <kut.akdogan@gmail.com>
2021-07-26 14:36:43 -0400

    Fix more outputs

--- modules/config-baseline/outputs.tf
+++ modules/config-baseline/outputs.tf
@@ -5,5 +5,5 @@ output "configuration_recorder" {
 
 output "config_sns_topic" {
   description = "The SNS topic that AWS Config delivers notifications to."
-  value       = var.enabled ? aws_sns_topic.config[0] : null
+  value       = var.enabled && var.sns_topic_name != "" ? aws_sns_topic.config[0] : null
 }
