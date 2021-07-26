7df7252021421eb8bf52534c71efb9296f4fb143
Kut Akdogan <kut.akdogan@gmail.com>
2021-07-26 14:01:55 -0400

    Delete some VPC outputs

--- modules/vpc-baseline/outputs.tf
+++ modules/vpc-baseline/outputs.tf
@@ -1,22 +1,22 @@
-output "default_vpc" {
-  description = "The default VPC."
-  value       = var.enabled ? aws_default_vpc.default[0] : null
-}
-
-output "default_security_group" {
-  description = "The default security group."
-  value       = var.enabled ? aws_default_security_group.default[0] : null
-}
-
-output "default_network_acl" {
-  description = "The default network ACL."
-  value       = var.enabled ? aws_default_network_acl.default[0] : null
-}
-
-output "default_route_table" {
-  description = "The default route table."
-  value       = var.enabled ? aws_default_route_table.default[0] : null
-}
+//output "default_vpc" {
+//  description = "The default VPC."
+//  value       = var.enabled ? aws_default_vpc.default[0] : null
+//}
+//
+//output "default_security_group" {
+//  description = "The default security group."
+//  value       = var.enabled ? aws_default_security_group.default[0] : null
+//}
+//
+//output "default_network_acl" {
+//  description = "The default network ACL."
+//  value       = var.enabled ? aws_default_network_acl.default[0] : null
+//}
+//
+//output "default_route_table" {
+//  description = "The default route table."
+//  value       = var.enabled ? aws_default_route_table.default[0] : null
+//}
 
 output "vpc_flow_logs_group" {
   description = "The CloudWatch Logs log group which stores VPC Flow Logs."
